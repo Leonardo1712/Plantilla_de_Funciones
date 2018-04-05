49cc34bc31d8428877e6dc25aa3f0bef753ff13a
Leonardo1712 <leo_gd17@hotmail.com>
2018-04-05 11:54:06 -0500

    Plantillas de Funciones

---
+++ Plantilla de Funciones/Plantilla de Funciones/Plantilla de Funciones.cpp
@@ -0,0 +1,45 @@
+// Plantilla de Funciones.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+//datos enteros (int)
+int min (int a, int b)
+{
+	if  (a <= b)
+		return a;
+	return b;
+}
+
+//datos largos (long)
+long min (long a, long b)
+{
+	if (a <= b)
+		return a;
+	return b;
+}
+
+//datos double (double)
+double min (double a, double b)
+{
+	if (a <= b)
+		return a;
+	return b;
+}
+
+int main()
+{
+	int ea = 1, eb = 5;
+	cout << "(int): " << min(ea, eb) << endl;
+
+	long la = 10000, lb = 4000;
+	cout << "(long): " << min(la, lb) << endl;
+
+	double da = 423.654, db = 789.10;
+	cout << "(double): " << min(da, db) << endl;
+	return 0;
+}
+
