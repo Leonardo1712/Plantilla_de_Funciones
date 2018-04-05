5989ede1a1a8624f8d0e8cd1ab5ee03bc877a976
Leonardo1712 <leo_gd17@hotmail.com>
2018-04-05 18:07:34 -0500

    Calculo de numero menor, con menu

--- Plantilla de Funciones/Plantilla de Funciones/Plantilla de Funciones.cpp
+++ Plantilla de Funciones/Plantilla de Funciones/Plantilla de Funciones.cpp
@@ -5,7 +5,7 @@
 #include <iostream>
 
 using namespace std;
-/*
+
 //datos enteros (int)
 int min (int a, int b)
 {
@@ -29,7 +29,7 @@ double min (double a, double b)
 		return a;
 	return b;
 }
-*/
+/*
 template <class T> T min (T a, T b)
 {
 	if(a <= b)
@@ -37,17 +37,52 @@ template <class T> T min (T a, T b)
 	else
 		return b;
 }
-
+*/
 int main()
 {
-	int ea = 1, eb = 5;
-	cout << "(int): " << min(ea, eb) << endl;
+	
+	int option;
 
-	long la = 10000, lb = 4000;
-	cout << "(long): " << min(la, lb) << endl;
+	int ea, eb;
+	long la, lb;
+	double da, db;
 
-	double da = 423.654, db = 789.10;
-	cout << "(double): " << min(da, db) << endl;
-	return 0;
+	do{
+		cout << "Seleccione el tipo de numero que desea calcular..." << endl;
+		cout << "1) int " << endl;
+		cout << "2) double " << endl;
+		cout << "3) long " << endl;
+		cout << "4) salir " << endl;
+
+		cin >> option;
+
+		switch (option)
+		{
+			case 1 : 
+				cout << "Inserte el primer numero de tipo int..." << endl;
+				cin>>ea;
+				cout << "Inserte el segundo numero de tipo int..." << endl;
+				cin>>eb;
+				cout << "El numero mas chico es: " << min(ea,eb) << endl;
+			break;
+			case 2 : 
+				cout << "Inserte el primer numero de tipo double..." << endl;
+				cin>>da;
+				cout << "Inserte el segundo numero de tipo double..." << endl;
+				cin>>db;
+				cout << "El numero mas chico es: " << min(da,db) << endl;
+			break;
+			case 3 : 
+				cout << "Inserte el primer numero de tipo long..." << endl;
+				cin>>la;
+				cout << "Inserte el segundo numero de tipo long..." << endl;
+				cin>>lb;
+				cout << "El numero mas chico es: " << min(la,lb) << endl;
+			break;
+
+		default: cout << "ERROR"  << endl;
+			break;
+		}
+	}while(option !=  4);
 }
 
